377e63340a723aaf15cdd54f6ed784584af6717c
Eric Skogen <audionerd@users.noreply.github.com>
2018-07-20 11:42:54 -0500

    remember last script path in prefs

--- src/js/window/main-window.js
+++ src/js/window/main-window.js
@@ -20,17 +20,14 @@ const prefs = require('../prefs')
 // display them
 // load settings prefs
 
-prefs.init(path.join(app.getPath('userData'), 'prefs.json'))
-console.log('Script Assistant v' + prefs.get('version'))
-
-let scriptPath = "/Users/setpixel/git/explorers-script/EXPLORERS.fountain"
+let scriptPath
 let outputDirectory = app.getPath('documents')
 
 const loadScript = (filepath) => {
   scriptPath = filepath
   document.querySelector('#revealScript').innerText = scriptPath
   loadStats(scriptPath)
-
+  prefs.set('lastScriptPath', scriptPath)
 }
 
 const loadStats = async (filepath, modifiedTime) => {
@@ -67,8 +64,6 @@ const loadStats = async (filepath, modifiedTime) => {
   document.querySelector('#stats').innerHTML = statsHTML.join('')
 }
 
-loadScript(scriptPath)
-
 
 
 const onLinkedFileChange = async (eventType, filepath, stats) => {
@@ -88,31 +83,27 @@ const onLinkedFileChange = async (eventType, filepath, stats) => {
   // display them
 }
 
+const init = () => {
+  prefs.init(path.join(app.getPath('userData'), 'prefs.json'))
+  console.log('Script Assistant v' + prefs.get('version'))
+  console.log('Most Recent Script Path:', prefs.get('lastScriptPath'))
 
-let watcher = chokidar.watch(null, {
-  disableGlobbing: true // treat file strings as literal file names
-})
-watcher.on('all', onLinkedFileChange)
+  if (prefs.get('lastScriptPath')) {
+    loadScript(prefs.get('lastScriptPath'))
+  }
 
-watcher.add(scriptPath)
+  let watcher = chokidar.watch(null, {
+    disableGlobbing: true // treat file strings as literal file names
+  })
+  watcher.on('all', onLinkedFileChange)
 
-
-// // const pdf = require('../pdf')
-
-// // pdf.generatePDF()
-
-// // console.log(document)
-
-
-ipcRenderer.on('ready', () => {
-})
+  watcher.add(scriptPath)
 
   document.querySelector('#revealScript').addEventListener('click', () => {
     if (scriptPath.length) {
       shell.showItemInFolder(scriptPath)
     }
   })
-
 
   document.querySelector('#generateScriptPdf').addEventListener('click', async () => {
     if (scriptPath.length) {
@@ -135,7 +126,6 @@ ipcRenderer.on('ready', () => {
     }
   })
 
-
   document.querySelector('#generateSceneList').addEventListener('click', async () => {
     if (scriptPath.length) {
       await generateSceneList.generate({
@@ -146,7 +136,7 @@ ipcRenderer.on('ready', () => {
       shell.showItemInFolder(path.join(outputDirectory, 'scene-list.csv'))
     }
   })
-
+}
 
 window.ondragover = () => { return false }
 window.ondragleave = () => { return false }
@@ -161,3 +151,7 @@ window.ondrop = e => {
     loadScript(file.path)
   }
 }
+
+ipcRenderer.on('ready', () => {})
+
+init()
