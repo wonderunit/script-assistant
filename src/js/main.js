a0dac230109cf9b99b306276e6474042a3ef84ce
Charles Forman <setpixel@gmail.com>
2018-07-24 09:18:43 -0400

    added move to applications folder

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,50 @@ const { ipcMain } = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
+const isDev = require('electron-is-dev')
+
 const path = require('path')
 const url = require('url')
+const os = require('os')
 
 let mainWindow
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true
+
+
+if (os.platform() === 'darwin') {
+  if (!isDev && !app.isInApplicationsFolder()) {
+    const choice = dialog.showMessageBox({
+      type: 'question',
+      title: 'Move to Applications folder?',
+      message: 'Would you like to move Storyboarder to the Applications folder?',
+      buttons: ['Move to Applications', 'Do Not Move'],
+      defaultId: 1
+    })
+  
+    const yes = (choice === 0)
+    
+    if (yes) {
+      try {
+        let didMove = app.moveToApplicationsFolder()
+        if (!didMove) {
+          dialog.showMessageBox(null, {
+            type: 'error',
+            message: 'Could not move to Applications folder'
+          })
+        }
+      } catch (err) {
+        dialog.showMessageBox(null, {
+          type: 'error',
+          message: err.message
+        })
+      }
+    }
+  }
+}
+
+
+
 
 function createWindow () {
 
